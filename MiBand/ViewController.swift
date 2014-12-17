9524a68a6576ada6ce81d7a21197a9b0641c7795
Paul Gavrikov <pg74@mars.uni-freiburg.de>
2014-12-18 00:23:10 +0100

    Initial upload. Supports Steps & Battery level

---
+++ MiBand/ViewController.swift
@@ -0,0 +1,177 @@
+/*
+Copyright (c) 2014 Paul Gavrikov
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import Cocoa
+import CoreBluetooth
+
+class ViewController: NSViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    var centralManager:CBCentralManager!
+    var discovered = false
+    var connectingPeripheral: CBPeripheral!
+    
+    
+    @IBOutlet weak var stepsView: NSTextField!
+    @IBOutlet weak var spinnerView: NSProgressIndicator!
+    @IBOutlet weak var batteryView: NSTextField!
+
+    @IBAction func refreshButton(sender: AnyObject) {
+                discoverDevices()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        spinnerView.startAnimation(nil)
+        startUpCentralManager()
+        
+    }
+    
+    func startUpCentralManager() {
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func discoverDevices() {
+        
+        if(connectingPeripheral != nil) {
+            centralManager.cancelPeripheralConnection(connectingPeripheral)
+        }
+        
+        stepsView.stringValue = "Searching"
+        batteryView.stringValue = ""
+        spinnerView.hidden = false
+        println("discovery")
+        centralManager.scanForPeripheralsWithServices(nil, options: nil)
+    }
+    
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: (NSDictionary), RSSI: NSNumber!) {
+        
+        //println("Discovered: " + peripheral.name)
+        if(peripheral.name == "MI") {
+            stepsView.stringValue = "Connecting to MI"
+            self.connectingPeripheral = peripheral
+            centralManager.stopScan()
+            self.centralManager.connectPeripheral(peripheral, options: nil)
+        } else {
+            println("skipped " + peripheral.name )
+        }
+        
+    }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager!) { //BLE status
+        var msg = ""
+        switch (central.state) {
+        case .PoweredOff:
+            msg = "CoreBluetooth BLE hardware is powered off"
+            println("\(msg)")
+            stepsView.stringValue = "Please turn on Bluetooth and retry"
+            
+        case .PoweredOn:
+            msg = "CoreBluetooth BLE hardware is powered on and ready"
+            if(!discovered) {
+                discovered = true
+                discoverDevices()
+            }
+            
+        case .Resetting:
+            var msg = "CoreBluetooth BLE hardware is resetting"
+            
+        case .Unauthorized:
+            var msg = "CoreBluetooth BLE state is unauthorized"
+            
+        case .Unknown:
+            var msg = "CoreBluetooth BLE state is unknown"
+            
+        case .Unsupported:
+            var msg = "CoreBluetooth BLE hardware is unsupported on this platform"
+            stepsView.stringValue = "Your Mac does not support BLE"
+            
+        }
+        output("State", data: msg)
+    }
+    
+    func centralManager(central: CBCentralManager!,didConnectPeripheral peripheral: CBPeripheral!)
+    {
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+        
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!)
+    {
+        println("peripherial services")
+        if let servicePeripherals = peripheral.services as? [CBService]
+        {
+            for servicePeripheral in servicePeripherals
+            {
+                peripheral.discoverCharacteristics(nil, forService: servicePeripheral)
+                
+            }
+            
+        }
+    }
+    
+    func refreshBLE() {
+        centralManager.scanForPeripheralsWithServices(nil, options: nil)
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
+      
+        if let charactericsArr = service.characteristics  as? [CBCharacteristic]
+        {
+            for cc in charactericsArr
+            {
+                peripheral.setNotifyValue(true, forCharacteristic: cc)
+                
+                if cc.UUID.UUIDString == "FF0F"{
+                    output("Characteristic", data: cc)
+                    let data: NSData = "2".dataUsingEncoding(NSUTF8StringEncoding)!
+                    peripheral.writeValue(data, forCharacteristic: cc, type: CBCharacteristicWriteType.WithoutResponse)
+                    output("Characteristic", data: cc)
+                } else if cc.UUID.UUIDString == "FF06" {
+                    println("READING STEPS")
+                    peripheral.readValueForCharacteristic(cc)
+                } else if cc.UUID.UUIDString == "FF0C" {
+                    println("READING BATTERY")
+                    peripheral.readValueForCharacteristic(cc)
+                }
+            }
+            
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didUpdateValueForCharacteristic characteristic: CBCharacteristic!, error: NSError!) {
+        
+        output("Data for "+characteristic.UUID.UUIDString, data: characteristic.value())
+        
+        if(characteristic.UUID.UUIDString == "FF06") {
+            spinnerView.hidden = true
+            var u16 = UnsafePointer<Int>(characteristic.value().bytes).memory
+            stepsView.stringValue = ("\(u16) steps")
+        } else if(characteristic.UUID.UUIDString == "FF0C") {
+            spinnerView.hidden = true
+            var u16 = UnsafePointer<Int32>(characteristic.value().bytes).memory
+            u16 =  u16 & 0xff
+            batteryView.stringValue = ("\(u16) % charged")
+        }
+        
+        
+    }
+    
+    func output(description: String, data: AnyObject){
+        println("\(description): \(data)")
+       // textField.text = textField.text + "\(description): \(data)\n"
+    }
+    
+    
+    
+
+    override var representedObject: AnyObject? {
+        didSet {
+        }
+    }
+}
+
